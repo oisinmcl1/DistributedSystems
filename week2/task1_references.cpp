74dc23f0313e581176151d343755b6c37c7edbf0
oisinmcl1 <omclaughlin99@gmail.com>
2026-01-22 19:30:37 +0000

    week 2 complete

--- week2/task1_references.cpp
+++ week2/task1_references.cpp
@@ -12,12 +12,63 @@ void modify_reference(int& x) {
     std::cout << "Inside modify_reference: " << x << "\n";
 }
 
+
+// Using reference - cannot be null, must be initialized
+void swap_ref(int& a, int& b) {
+    int temp = a;
+
+    a = b;
+    b = temp;
+}
+
+// Using pointers - can be null, can be reassigned
+void swap_ptr(int* a, int* b) {
+    if (a == nullptr || b == nullptr) {
+        std::cout << "Error: Null pointer passed to swap_ptr.\n";
+        return;
+    }
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
 int main() {
     int a = 42;
-    std::cout << "Original value: " << a << "\n";
+
+    std::cout << "Original value: " << a << "\n\n";
+
     modify_value(a);
-    std::cout << "After modify_value: " << a << "\n";
+    std::cout << "After modify_value: " << a << "\n\n";
+
     modify_reference(a);
-    std::cout << "After modify_reference: " << a << "\n";
+    std::cout << "After modify_reference: " << a << "\n\n";
+
+
+    // Part B
+    int x = 10, y = 20;
+
+    std::cout << "Before swap_ref: x=" << x << ", y=" << y << "\n";
+    swap_ref(x, y);
+    std::cout << "After swap_ref: x=" << x << ", y=" << y << "\n\n";
+
+    int p = 30, q = 40;
+
+    std::cout << "Before swap_ptr: p=" << p << ", q=" << q << "\n";
+    swap_ptr(&p, &q);
+    std::cout << "After swap_ptr: p=" << p << ", q=" << q << "\n\n";
+
+    // Uninitialised reference:
+    // int& ref;
+
+    // Passing NULL pointer to swap_ptr
+    std::cout << "Passing NULL pointer to swap_ptr:\n";
+    swap_ptr(nullptr, nullptr);
+
+    // Create null pointer, dereference it
+    int* null_ptr = nullptr;
+    std::cout << *null_ptr << "\n"; // Undefined behavior?
+
     return 0;
 }
--- week2/task2_stack_heap.cpp
+++ week2/task2_stack_heap.cpp
@@ -2,36 +2,44 @@
 #include <chrono>
 
 class Object {
+
 private:
     int id;
     int data[100];  // Some data to make it non-trivial
+
 public:
     Object(int id) : id(id) {
-        std::cout << "  [Constructor] Object " << id << " created\n";
+        std::cout << "  [Constructor] Object " << id << " created\n\n";
     }
-    
+
     ~Object() {
-        std::cout << "  [Destructor] Object " << id << " destroyed\n";
+        std::cout << "  [Destructor] Object " << id << " destroyed\n\n";
     }
-    
+
     void doWork() {
         data[0] = id * 10;
+        std::cout << "Object " << id << " is doing work: data[0] = " << data[0] << "\n\n";
     }
 };
 
 void stack_example() {
-    std::cout << "\n=== Stack Allocation ===\n";
-    std::cout << "Creating stack object...\n";
+    std::cout << "\n=== Stack Allocation ===\n\n";
+    std::cout << "Creating stack object...\n\n";
+
     Object obj1(1);
     obj1.doWork();
-    std::cout << "End of function - obj1 will be destroyed automatically\n";
+
+    std::cout << "End of function - obj1 will be destroyed automatically\n\n";
 }
+
 
 void heap_example() {
     std::cout << "\n=== Heap Allocation ===\n";
     std::cout << "Creating heap object...\n";
+
     Object* obj2 = new Object(2);
     obj2->doWork();
+
     std::cout << "End of function - obj2 still exists!\n";
     // Memory leak if we don't delete!
 }
@@ -39,17 +47,21 @@ void heap_example() {
 void heap_with_cleanup() {
     std::cout << "\n=== Heap with Proper Cleanup ===\n";
     std::cout << "Creating heap object...\n";
+
     Object* obj3 = new Object(3);
     obj3->doWork();
+
     std::cout << "Manually deleting object...\n";
     delete obj3;
+
     std::cout << "End of function - obj3 properly cleaned up\n";
 }
+
 
 void performance_test() {
     std::cout << "\n=== Performance Test ===\n";
     const int iterations = 1000000;
-    
+
     // Stack allocation timing
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < iterations; i++) {
@@ -57,7 +69,7 @@ void performance_test() {
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto stack_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    
+
     // Heap allocation timing
     start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < iterations; i++) {
@@ -66,25 +78,27 @@ void performance_test() {
     }
     end = std::chrono::high_resolution_clock::now();
     auto heap_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    
+
     std::cout << "Stack: " << stack_time.count() << "ms\n";
     std::cout << "Heap:  " << heap_time.count() << "ms\n";
-    std::cout << "Heap is ~" << (heap_time.count() / (float)stack_time.count()) 
+    std::cout << "Heap is ~" << (heap_time.count() / (float)stack_time.count())
               << "x slower\n";
 }
+
 
 int main() {
     std::cout << "Program started\n";
     stack_example();
-    std::cout << "Back in main - stack object is already destroyed\n";
-    
+
+    std::cout << "Back in main - stack object is already destroyed\n\n";
+
     heap_example();
     std::cout << "Back in main - heap object still alive (leaked!)\n";
-    
+
     heap_with_cleanup();
     std::cout << "Back in main - heap object properly destroyed\n\n";
-    
+
     performance_test();
-    
+
     return 0;
 }
